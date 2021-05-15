be7d48a25bd97a794d760adcf6244ab96c32c5c6
nonsodaniel <nonsodaniel07@gmail.com>
2021-05-15 06:52:01 +0100

    First commit

---
+++ app.js
@@ -0,0 +1,59 @@
+var message = document.querySelector("#note");
+
+var SpeechRecognition = SpeechRecognition || webkitSpeechRecognition;
+var SpeechGrammarList = SpeechGrammarList || webkitSpeechGrammarList;
+const synth = window.speechSynthesis;
+var grammar = "#JSGF V1.0;";
+var final = "";
+var recognition = new SpeechRecognition();
+var speechRecognitionList = new SpeechGrammarList();
+speechRecognitionList.addFromString(grammar, 1);
+recognition.grammars = speechRecognitionList;
+recognition.lang = "en-US";
+recognition.interimResults = true;
+recognition.continuous = true;
+recognition.onresult = function (event) {
+  const transcript = Array.from(event.results)
+    .map((result) => result[0])
+    .map((result) => result.transcript)
+    .join("");
+  message.value = transcript;
+};
+
+recognition.onerror = function (event) {
+  message.textContent = "Error occurred in recognition: " + event.error;
+};
+
+recognition.addEventListener("soundstart", function () {
+  console.log("Some sound is being received");
+});
+recognition.addEventListener("speechend", function () {
+  console.log("Some sound has stopped");
+});
+const copyPaseText = () => {
+  var copyText = document.querySelector("#note").value;
+  navigator.clipboard.writeText(copyText).then(
+    function () {
+      alert(" Copying to clipboard was successful!");
+    },
+    function (err) {
+      console.error("Async: Could not copy text: ", err);
+    }
+  );
+};
+const clearNote = () => {
+  message.value = "";
+};
+
+document.querySelector("#micStart").addEventListener("click", () => {
+  recognition.start();
+});
+document.querySelector("#micStop").addEventListener("click", function () {
+  recognition.stop();
+});
+document.querySelector("#copyTxt").addEventListener("click", function () {
+  copyPaseText();
+});
+document.querySelector("#clearNote").addEventListener("click", function () {
+  clearNote();
+});
